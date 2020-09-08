9414edb1491a334d2559eea6312b85419361ba0b
luweishuang <pifuchan@gmail.com>
2020-09-08 16:16:49 +0800

    add doc

--- geek_setup.sh
+++ geek_setup.sh
@@ -6,5 +6,18 @@ mkdir -p data/asr/mandarin
 cd /data/asr/mandarin
 unzip -d /input/data/asr/mandarin all1.zip
 
-cd /root/code/asr/fairseq/examples/wav2vec
-mkdir -p output
+cd /root
+mkdir -p code/asr
+cd /root/code/asr
+cp -r /data/code/asr/fairseq .
+cp -r /data/code/asr/apex .
+
+# install apex and fairseq by hand
+#cd apex
+#pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext"  --global-option="--deprecated_fused_adam" --global-option="--xentropy" --global-option="--fast_multihead_attn" ./
+
+#cd ../fairseq
+#pip install --editable ./ -i https://pypi.doubanio.com/simple
+
+
+
